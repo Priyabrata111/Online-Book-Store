a517aae3440dfcd02e498b0a1a8044ec7afbe938
Priyabrata Mondal <priyabratamondal622@gmail.com>
2024-02-26 23:41:14 +0530

    Started working on Frontend Part of our Project with react.js

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+const App = () => {
+  return (
+    <>
+      <h1 className="open-sans-p">Book Store</h1>
+    </>
+  );
+};
+
+export default App;
